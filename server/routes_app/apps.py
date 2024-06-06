416701d0477e367aa9bcfb1100a79964af1999ac
Linqing <62589604+Lanceart@users.noreply.github.com>
2024-06-05 17:23:36 -0700

    step: clean old backend

--- server/routes_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class RoutesAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'routes_app'
--- server/routes_app/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Profile
-
-class ProfileSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Profile
-        fields = ['first_name', 'last_name', 'email', 'role']  
